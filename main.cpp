5512a476579e1ae55cd7d84ec56a945c4a7cbf3d
Peter Chukwuemeka Nwanosike <dexter4life@gmail.com>
2018-09-23 00:35:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,13 @@
 
 typedef unsigned int  ip_addr;
 
-IP_ADAPTER_INFO *getAdapterInfoForAddress(ip_addr in)
+IP_ADAPTER_INFO getAdapterInfoForAddress(ip_addr in)
 {
-	IP_ADAPTER_INFO *pAdapterInfo = 0;
+	IP_ADAPTER_INFO *pAdapterInfo = 0, found;
 	ULONG dataSize = 0;
+	
+	memset(&found, 0, sizeof(IP_ADAPTER_INFO));
+
 	//call GetAdaptersInfo to fill required size;
 
 	if (GetAdaptersInfo(pAdapterInfo, &dataSize) == ERROR_BUFFER_OVERFLOW) {
@@ -21,20 +24,21 @@ IP_ADAPTER_INFO *getAdapterInfoForAddress(ip_addr in)
 
 		pAdapterInfo = new IP_ADAPTER_INFO[dataSize];
 		memset(pAdapterInfo, 0, sizeof(IP_ADAPTER_INFO)*dataSize);
+	
 		GetAdaptersInfo(pAdapterInfo, &dataSize);
 	}
 	
 	//still nothing happened.
-	if (!pAdapterInfo) return 0;
+	if (!pAdapterInfo) return found;
 	
 	for (IP_ADAPTER_INFO* Current = pAdapterInfo; Current != NULL; Current = Current->Next)
 	{
 		if (in == inet_addr(Current->IpAddressList.IpAddress.String)) {
-			return Current;
+			memcpy(&found, Current, sizeof(IP_ADAPTER_INFO));
 		}
 	}
 	//we didn't find it so return
-	return 0;
+	return found;
 }
 
 
